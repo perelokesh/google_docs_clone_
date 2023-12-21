3829b94dc7e339947ad8806c42bd1d4336793d6c
LOKESH <lokesh@LOKESHs-MacBook-Air.local>
2023-12-22 00:21:27 +0530

    added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Signin from './components/Signin';
+import Document from './components/Document';
 function App() {
-
-  return (
-    <>
-    </>
-  )
+return(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Signin />}/>
+      <Route path="/document" element= {<Document/>}/>
+    </Routes>
+  </Router>
+)
+ 
 }
 
 export default App
--- client/src/components/Document.jsx
+++ client/src/components/Document.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Header() {
+export default function Document() {
   return (
     <div>
-      header
+      Document page
     </div>
   )
 }
